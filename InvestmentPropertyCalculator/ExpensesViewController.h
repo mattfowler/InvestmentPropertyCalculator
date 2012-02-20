48b4a4c3039f2240fbecd828582465e661d1530a
Matt Fowler <matt.fow@gmail.com>
2012-02-20 13:29:08 -0500

    Added other views for tabs and added a group for views.

---
+++ InvestmentPropertyCalculator/ExpensesViewController.h
@@ -0,0 +1,13 @@
+//
+//  ExpensesViewController.h
+//  InvestmentPropertyCalculator
+//
+//  Created by Matt Fowler on 2/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExpensesViewController : UIViewController
+
+@end
