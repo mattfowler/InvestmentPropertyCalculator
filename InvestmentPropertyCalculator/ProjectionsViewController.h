b741dd4ad6eaee862721e85630d97a396d75d4b4
Matt Fowler <matt.fow@gmail.com>
2012-02-25 19:21:08 -0500

    Added projections view.  Removing taxes field from main calculator and doing tax bracket based calculations instead.

---
+++ InvestmentPropertyCalculator/ProjectionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProjectionsViewController.h
+//  InvestmentPropertyCalculator
+//
+//  Created by Matt Fowler on 2/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProjectionsViewController : UIViewController
+
+@end
