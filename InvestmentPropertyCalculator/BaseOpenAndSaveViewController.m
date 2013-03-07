5724209b6f21a7ab380ac26b42d63d8ac5272418
mattfowler <matt.fow@gmail.com>
2013-03-07 13:51:00 -0500

    Default navigation bar title to unsaved property for now and properly link up the compare and projection views nav bar to their file's owner.

--- InvestmentPropertyCalculator/BaseOpenAndSaveViewController.m
+++ InvestmentPropertyCalculator/BaseOpenAndSaveViewController.m
@@ -23,6 +23,7 @@
 }
 
 - (void) viewDidLoad {
+    self.navigationBar.topItem.title = @"Unsaved Property";
     fileManager = [[PropertyFileManager alloc] init];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
